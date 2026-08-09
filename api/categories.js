5e57be8926bb0db4c26065738ed270bb54df0402
adam0865 <adam0865@gmail.com>
2026-08-10 01:49:24 +0700

    Fix API import paths: require('../db') -> require('./db')

--- api/categories.js
+++ api/categories.js
@@ -1,4 +1,4 @@
-const { getSupabaseClient } = require('../db');
+const { getSupabaseClient } = require('./db');
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
